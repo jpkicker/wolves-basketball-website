9c36cb5d408e5add9190c7c764034c1189bc2666
Devon <devon@agents.local>
2026-05-02 15:03:28 +0000

    Add May 8-10 Tavares game schedule (4 games)

--- src/data/schedule2026.js
+++ src/data/schedule2026.js
@@ -251,7 +251,13 @@ export const tournaments = [
       address: "400 E Alfred St, Tavares, FL 32778"
     },
     hotelLink: null,
-    details: "🏨 Hotel: Hampton Inn Leesburg/Tavares, 9630 US Highway 441, Leesburg, FL 34788. Check-in 5/8, Check-out 5/10 (2 nights). Feel free to book through Expedia, Hotels.com, or any service you prefer."
+    details: "🏨 Hotel: Hampton Inn Leesburg/Tavares, 9630 US Highway 441, Leesburg, FL 34788. Check-in 5/8, Check-out 5/10 (2 nights). Feel free to book through Expedia, Hotels.com, or any service you prefer.",
+    games: [
+      { date: "05/09", time: "11:15 AM", opponent: "Team Khalfani 17U (FL)", court: "Court 1", homeAway: "Home" },
+      { date: "05/09", time: "4:40 PM", opponent: "Orlando Bad Boys 17U (FL)", court: "Court 1", homeAway: "Away" },
+      { date: "05/10", time: "10:10 AM", opponent: "Kissimmee AllStars 17U", court: "Court 3", homeAway: "Home" },
+      { date: "05/10", time: "12:20 PM", opponent: "Miami Basketball Club 17U (FL)", court: "Court 1", homeAway: "Away" }
+    ]
   },
   {
     id: 8,
